21e868a086e4249c2e644b708ee542f6f5967b76
MarcinParda <marcin98parda@gmail.com>
2023-02-12 22:00:13 +0100

    feat: change font to Noto_Serif_Khojki

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,4 +1,4 @@
-import { Inter } from '@next/font/google'
+import { Noto_Serif_Khojki } from '@next/font/google'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import { ReactNode } from 'react'
@@ -8,14 +8,14 @@ interface Props {
   children: ReactNode
 }
 
-const inter = Inter({
-  subsets: ['latin'],
+const font = Noto_Serif_Khojki({
+  subsets: ['latin-ext'],
 })
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
+      <div className={`${font.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
